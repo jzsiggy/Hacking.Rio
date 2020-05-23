0a1967c78234c96ea21b53a32245ee18bd07d61b
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-23 10:19:57 -0300

    Removing and modifying default CRA vals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/form/Form';
+import Home from './components/home/Home';
+import Done from './components/done/Done';
+
+
+import { Route , Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/forms" component={Form} />
+      <Route path="/done" component={Done} />
+    </Switch>
   );
 }
 
